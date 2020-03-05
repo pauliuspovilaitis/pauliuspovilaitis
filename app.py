2a8ddbcea2d886834741510c1559851cb9233081
Paulius Povilaitis (admin) <_paulius.povilaitis@outokumpu.com>
2020-03-05 11:16:43 +0200

    sys2k and 404 handling

--- app.py
+++ app.py
@@ -8,8 +8,12 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 os.environ['PATH'] = config.ENV_CONFIG['path']
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({'data': str('wrong URL')})
+       
 @app.route('/pf/<dbTable>', methods=['GET'])
-def index(dbTable):
+def pf(dbTable):
     connection = None    
     try:    
         connection = cx_Oracle.connect(config.DATABASE_CONFIG_PF['user'], 
@@ -23,6 +27,22 @@ def index(dbTable):
         return jsonify({'data': items})  
     except Exception as e:  
         return jsonify({'data': str(e)}) 
-      
+                    
+@app.route('/sys2k/<dbTable>', methods=['GET'])        
+def sys2k(dbTable):
+    connection = None    
+    try:    
+        connection = cx_Oracle.connect(config.DATABASE_CONFIG_SYS2K['user'], 
+                                       config.DATABASE_CONFIG_SYS2K['password'], 
+                                       config.DATABASE_CONFIG_SYS2K['host'])
+        cur = connection.cursor()
+        result = cur.execute(f"""SELECT * FROM {dbTable}""")
+        items = [dict(zip([key[0] for key in cur.description], row )) for row in result]
+        cur.close()
+        connection.close()    
+        return jsonify({'data': items})  
+    except Exception as e:  
+        return jsonify({'data': str(e)})         
+             
 if __name__ == '__main__':
    app.run(debug=False)
